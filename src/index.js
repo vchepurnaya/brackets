c69df8512d1aaccbee1499615d89a8c06cc4860c
Vika Chepurnaya <v.chepurnayaw@gmail.com>
2021-03-14 01:43:52 +0300

    feat: add js code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let newConfig = [];
+    let string = str;
+
+    bracketsConfig.forEach((item, index) => {
+        newConfig.push(bracketsConfig[index][0] + bracketsConfig[index][1]);
+    })
+
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < newConfig.length; j++) {
+            string = string.replace(newConfig[j], '');
+        }
+
+        if (!string.length) {
+            return true;
+        }
+    }
+
+    return false;
 }
